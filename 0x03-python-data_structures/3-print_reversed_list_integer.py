d6ce196a5c5bc240b778eebcbe6a54a6ec8cdf60
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-01-11 01:43:10 +0200

    Commit

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    size = len(my_list)
-    if size > 0
-        for i in range(size, 0, -1):
-            print("{}".format(my_list[i}))
+    if my_list is None:
+        return None
+    for i in my_list[::-1]:
+        print("{:d}".format(i))
