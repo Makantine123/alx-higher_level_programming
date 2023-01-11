f2cd7eb101a408a322b60c966b0a48cec71809f3
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-01-11 02:23:37 +0200

    Commit

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            print("{:d}".format(matrix[i][j]), end='')
+            if j != len(matrix[i]):
+                print(" ", end='')
+        print()
