ec7ce4c13b348f4f204e3349d7d76cb951c65e0c
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-01-11 02:33:22 +0200

    Commit

--- 0x03-python-data_structures/6-main.py
+++ 0x03-python-data_structures/6-main.py
@@ -2,9 +2,9 @@
 print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
 
 matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+    [1, 2, 3, 5],
+    [4, 5, 6, 5],
+    [7, 8, 9, 5]
 ]
 
 print_matrix_integer(matrix)
