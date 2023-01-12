775a93311c709f6fa758ea0eff58d77122d45f7e
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-01-12 18:49:05 +0200

    Commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    numArg = len(sys.argv) - 1
+    if (numArg == 0):
+        print("{:d} arguments".format(numArg))
+    else:
+        print("{:d} arguments".format(numArg))
+        i = 1
+        for i in range(numArg):
+            print("{:d}: {}".format(i + 1, sys.argv[i + 1]))
