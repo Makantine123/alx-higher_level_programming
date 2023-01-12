cf1442e52df38848b3a3196f5be79e5b3fca4755
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-01-12 17:16:28 +0200

    Commit

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    if idx > len(my_list) - 1 or idx < 0
+    if idx > len(my_list) - 1 or idx < 0:
         return my_list
     del my_list[idx]
     return my_list
