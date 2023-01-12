e539e936b67ec5d53917740aab5d055a668a68d9
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-01-12 16:37:14 +0200

    Commit

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    j = 0
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-            new_list[j] = my_list[i]
-            j += j
+            new_list[i] = True
+        else:
+            new_list[i] = False
     return new_list
