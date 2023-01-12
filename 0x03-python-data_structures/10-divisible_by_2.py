ddc9179d1d106a3c23f86d778f0da636af423b9d
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-01-12 16:34:04 +0200

    Commit

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    j = 0
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            new_list[j] = my_list[i]
+            j += j
+    return new_list
