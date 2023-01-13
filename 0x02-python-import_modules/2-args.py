3c91b55639ff62bfbae9c0a686f4852dada6d405
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-01-13 21:31:56 +0200

    Commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     import sys
     numArg = len(sys.argv) - 1
     if (numArg == 0):
-        print("{:d} arguments".format(numArg))
+        print("{:d} argument:".format(numArg))
     else:
-        print("{:d} arguments".format(numArg))
+        print("{:d} argument:".format(numArg))
         i = 1
         for i in range(numArg):
             print("{:d}: {}".format(i + 1, sys.argv[i + 1]))
