1f84be08989ced11831268d2930a846ea28300a9
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-01-14 07:42:03 +0200

    Commit

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is None:
+        return
     for in sorted(a_dictionary.keys()):
         print("{}: {}".format(i, a_dictionary[i]))
