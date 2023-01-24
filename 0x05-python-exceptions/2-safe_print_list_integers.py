f588f6e40f0c037a7e6ce6f37e1b589477ee5378
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-01-24 10:11:45 +0200

    Commit

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except (ValueError, TypeError, IndexError):
+            print("")
+            return (count)
+        else:
+            count += 1
+    print("")
+    return (count)
