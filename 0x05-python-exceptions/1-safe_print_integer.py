1f833ea0dee61166aa3ed0ed8fb5832b16d0e6ae
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-01-24 09:48:33 +0200

    Commit

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-        try:
-            print("{:d}".format(value))
-            return True
-        except (ValueError, TypeError):
-            return False
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (ValueError, TypeError):
+        return (False)
