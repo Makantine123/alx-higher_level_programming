8b8ac94f1d67756181f12cd4745b9741de3bd032
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-01-24 09:17:31 +0200

    Commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    if my_list:
+        for i in range(x):
+            try:
+                print(my_list[i], end="")
+            except IndexError:
+                print("")
+                return (count)
+            else:
+                count += 1
+    print("")
+    return (count)
