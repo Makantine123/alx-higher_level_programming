6486ce7717d49bf1c215b3d2357bad9bf8d13cca
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-01-29 22:52:14 +0200

    Commit

---
+++ 0x02-python-import_modules/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    newMatrix = []
+
+    for i in range(matrix):
+        print("{}".format(matrix[i]))
---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,7 @@
+#include <Python.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
