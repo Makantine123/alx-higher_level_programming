89ec8f750305d5148056db8fc0a67bf2268a89be
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-01-30 15:52:43 +0200

    Commit

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a class Square, based on 3-square.py"""
+
+
+class Square:
+    """Class square contains size, raises type and value errors, return area, has property"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of a square"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
