44c11ceb49f80176918706218a8e18b9c2f9702d
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-02 12:27:54 +0200

    Commit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -29,7 +29,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     @property
     def position(self):
         """The position property."""
