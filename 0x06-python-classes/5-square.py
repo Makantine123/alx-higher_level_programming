4397efcde79cc8c5187e8e5ed74105b74d6f38dd
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-02 10:53:01 +0200

    Commit

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -36,5 +36,5 @@ class Square:
         else:
             for i in range(self.__size):
                 for j in range(self.__size):
-                    print("#", end=' ')
+                    print("#", end='')
                 print()
