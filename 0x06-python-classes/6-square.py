edad4e9ecf74a89a6a0c754ccd323d528f6f82a0
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-02 12:06:05 +0200

    Commit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Class square contains size, raises type and value errors, return area,
     has property, prints to the stdout with #"""
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
@@ -29,12 +29,27 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    def position(self):
+        """The position property."""
+        return self._position_position
+
+    @position.setter
+    def position(self, value):
+        """Set the position of the square"""
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self._position = value
+
     def my_print(self):
         """Prints to standard output the square using # else and empty line"""
         if self.__size == 0:
             print()
         else:
             for i in range(self.__size):
+                for k in range(self.__position[0]):
+                    print("_", end='')
+
                 for j in range(self.__size):
                     print("#", end='')
                 print()
+
