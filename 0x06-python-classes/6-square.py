91bda853db4ee71ec0b3681f26580c5687378cfa
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-03 04:17:59 +0200

    Commit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,6 +11,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+        self.__position = position
 
     def area(self):
         """ area """
