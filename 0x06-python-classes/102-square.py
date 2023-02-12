6da68d72e89ce1d4f00a0b26f43bd4ad7a2dfcc8
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-12 20:47:16 +0200

    Commit

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,51 +1,53 @@
 #!/usr/bin/python3
-"""Defines a class Square, based on 4-square.py"""
+"""
+Square Class: defines a square by : (based on 2-square.py)
+"""
 
 
 class Square:
-    """Class square contains size, raises type and value errors, return area,
-    has property"""
+    """ class Square that defines a square """
     def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        """ Initialize attributes"""
+        self.size = size
 
     @property
     def size(self):
-        """size"""
+        """ gets the size"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Set the size of a square"""
+        """ sets the size with safe Assignment"""
         if type(value) is not int:
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
+            raise TypeError('size must be an integer')
+        if (value < 0):
+            raise ValueError('size must be >= 0')
         self.__size = value
 
     def area(self):
-        """Area"""
+        """ Return the area of the square"""
         return (self.__size * self.__size)
 
-    def __lteq__(self, other):
-        """Less than equal too comparison"""
-        return self.size <= other.size
+    def __lt__(self, other):
+        """ less than comparison """
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """ less than or equal than comparison """
+        return self.area() <= other.area()
 
     def __eq__(self, other):
-        """Equal to comparison"""
-        return self.size == other.size
+        """ equal to comparison """
+        return self.area() == other.area()
 
-    def __nteq__(self, other):
-        """Not Equal to comparison"""
-        return self.size != other.size
+    def __ne__(self, other):
+        """ not equal comparison """
+        return self.area() != other.area()
 
     def __gt__(self, other):
-        """Greater than comparison"""
-        return self.size > other.size
+        """ greater than """
+        return self.area() > other.area()
 
-    def __lt__(self, other):
-        """Less than equal comparison"""
-        return self.size < other.size
+    def __ge__(self, other):
+        """ greater than or equal to """
+        return self.area() >= other.area()
