47688069d1de9f9fc58e6bada0057dbc3a8e9c9b
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-12 21:13:34 +0200

    Commit

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Magic class from a given ByteCode"""
+import math
+
+
+class MagicClass:
+    """Initialization of the MagicClass"""
+    def __init__(self, radius=0):
+        """Initialization of data"""
+        self._MagicClass__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self._MagicClass__radius = radius
+
+    def area(self):
+        return self._MagicClass_radius ** 2 * math.pi
+
+    def circumference(self):
+        return 2 * math.pi * self._MagicClass__radius
