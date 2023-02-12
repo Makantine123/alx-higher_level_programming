a0db348483fe109d408883f4a2e1a7ade9c06a41
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-12 20:51:59 +0200

    Commit

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,34 +1,33 @@
-#!/usr/bin/python3
-"""
-Square Class: defines a square by : (based on 2-square.py)
-"""
+ #!/usr/bin/python3
+"""Defines a class Square, based on 3-square.py"""
 
 
 class Square:
-    """ class Square that defines a square """
+    """Class square contains size, raises type and value errors, return area,
+    has property"""
     def __init__(self, size=0):
-        """ Initialize attributes"""
-        self.size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
 
     @property
     def size(self):
-        """ gets the size"""
+        """size"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ sets the size with safe Assignment"""
+        """Set the size of a square"""
         if type(value) is not int:
-            raise TypeError('size must be an integer')
-        if (value < 0):
-            raise ValueError('size must be >= 0')
-        self.__size = value
-
-    def area(self):
-        """ Return the area of the square"""
-        return (self.__size * self.__size)
-
-    def __lt__(self, other):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value   def __lt__(self, other):
         """ less than comparison """
         return self.area() < other.area()
 
