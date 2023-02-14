7b570467d26d51e2f2fa43bdb1abfc4770d4828b
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-14 16:01:53 +0200

    Commit

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Rectangle Class based on 0-rectangle.py"""
+
+
+class Rectangle:
+    """Defines a rectangle class"""
+    def __init__(self, width=0, height=0):
+        """
+        Initialization of class object
+        Args:
+            width: Width of rectangle
+            height: Height of rectangle
+        Returns:
+            Nothing
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """The width of a rectangle"""
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        """The width setter of a rectangle"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+    @property
+    def height(self):
+        """The height property of a square"""
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        """The height setter of a rectangle"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self._height = value
+        self._height = value
