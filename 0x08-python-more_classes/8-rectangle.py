fea28f0f9950fc348cb5f1968cd0a0db8780eb5d
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-14 17:31:40 +0200

    Commit

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -85,7 +85,7 @@ class Rectangle:
     def bigger_or_equal(rect_1, rect_2):
         """Checks abd returns the bigger rectangle"""
         if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectanle")
+            raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
