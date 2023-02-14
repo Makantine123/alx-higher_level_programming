14e88e8b8178aba0a56a4be44375dd1ae1320a40
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-14 23:55:17 +0200

    Commit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Adding integers and floats module"""
+
 
 def add_integer(a, b=98):
+    """Defines a addition function"""
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
