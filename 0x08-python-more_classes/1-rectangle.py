37faa4d4b7cbcec9f8d5cac6eeab70a17b66ae06
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-14 16:06:18 +0200

    Commit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,13 +13,13 @@ class Rectangle:
         Returns:
             Nothing
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         """The width of a rectangle"""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -28,12 +28,12 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
         """The height property of a square"""
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -42,5 +42,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
-        self._height = value
+        self.__height = value
