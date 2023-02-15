6715717f7b6f80f4ef3013ada9cc82d7a9d11553
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-15 21:47:56 +0200

    Commit

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module defines function that divides all elements of matrix"""
+
+
+def matrix_divided(matrix, div):
+    """Function that divides all elements of a matrix"""
+    if not isinstance(matrix, list):
+        TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not (isinstance(div, float) or isinstance(div, int)):
+        TypeError("Each row of the matrix must have the same size")
+    if div == 0:
+        ZeroDivisionError("division by zero")
+    nmatrix = []
+    for i in range(len(matrix[0])):
+        for k in range(i):
+            nmatrix[i][k] = round(matrix[i][k] / div, 2)
+    return nmatrix
