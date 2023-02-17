35ec652a90e164907e0d9fc6039dc0eda15002e9
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-17 21:57:12 +0200

    Commit

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -12,5 +12,5 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     for i in range(size):
         for k in range(size):
-            print("#")
+            print("#",end="")
         print()
