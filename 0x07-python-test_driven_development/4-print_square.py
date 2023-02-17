d0426cd201b5758877a62443d6d40bf392a2570c
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-17 22:03:29 +0200

    Commit

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -5,12 +5,14 @@
 def print_square(size):
     """Function prints a square with character #"""
     if isinstance(size, float) and size < 0:
-        raise TyepError("size must be an integer")
+        raise TypeError("size must be an integer")
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
     for i in range(size):
         for k in range(size):
-            print("#",end="")
-        print()
+            if k != size:
+                print("#", end="")
+            else:
+                print("#")
