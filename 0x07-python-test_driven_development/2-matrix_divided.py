969ea515551483c0315d54de1a165d863818cd78
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-17 12:45:14 +0200

    Commit

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,7 +7,7 @@ def matrix_divided(matrix, div):
     if not isinstance(matrix, list):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats") 
     if not (isinstance(div, float) or isinstance(div, int)):
-        raise TypeError("Each row of the matrix must have the same size")
+        raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     nmatrix = []
