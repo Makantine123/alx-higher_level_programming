1a44c530969cd353487b314d6eb35895eef6f20b
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-17 18:25:23 +0200

    Commit

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -15,4 +15,4 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a sting")
 
-    print("My name is {:s} {:s}".format(fisrt_name, last_name))
+    print("My name is {:s} {:s}".format(first_name, last_name))
