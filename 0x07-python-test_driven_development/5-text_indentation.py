208db4364de45ff05fdc8ccb4dc65936c5a9324e
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-18 10:32:15 +0200

    Commit

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Module contain function which prints text """
+
+
+def text_indentation(text):
+    """ 
+    Function prints a text with 2 new lines after each of these characters
+    . , ? , :
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    
+    flag = 0
+    
+    for i in range(len(text)):
+        if flag == 1:
+            flag = 0
+            continue
+        if text[i] in [".", "?", ":"]:
+            print(text[i], end="")
+            print("")
+            if text[i + 1] == " ":
+                flag = 1
+        else:
+            print(text[i], end="")
