9b33f0b4105d7cef255820f853f5be65fde35060
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-22 01:37:48 +0200

    Commit

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(l=[]):
-    l.append("BestSchool")
-    return ", ".join(l)
+def magic_string(listN=[]):
+    listN.append("BestSchool")
+    return ", ".join(listN)
