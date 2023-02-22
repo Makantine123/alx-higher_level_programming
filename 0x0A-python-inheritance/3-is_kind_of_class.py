65ffc5bd3e8ccfe7bdc678829a4486ae0a4d22d5
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-22 11:37:20 +0200

    Commit

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module Function """
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns True if obj is instance of inherited class from a_class"""
+    if issubclass(obj, a_class):
+        return True
+    return False
