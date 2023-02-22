c570b647037c0356a5cde1b72557afec442c6e2a
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-22 11:27:11 +0200

    Commit

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,6 +6,6 @@ of the specified class. otherwise False
 
 def is_same_class(obj, a_class):
     """ function returns True if object is an instance of class"""
-    if isinstance(obj, a_class):
+    if type(obj) == a_class:
         return True
     return False
