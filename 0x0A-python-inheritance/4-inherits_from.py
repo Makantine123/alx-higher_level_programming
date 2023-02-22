66078484adddd78d52f8096a2ac50d49143f1de9
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-22 11:47:57 +0200

    Commit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,6 @@ def inherits_from(obj, a_class):
     """
     Returns True if the object is an instance class that inhererited a_class
     """
+    if (type(obj) == a_class):
+        return False
     return (isinstance(obj, a_class))
