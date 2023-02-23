1d9dba05e7e3ee9881a6b4b4a9cbb3cb82bf6e8f
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-23 18:23:33 +0200

    Commit

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Square Class inherits Rectangle
+"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+""" Program that creates a Square and instance its values """
+
+
+class Square(Rectangle):
+    """Class Rectangle, inherits from BaseGeometry"""
+    def __init__(self, size):
+        """ Initialization of rectangle class """
+        self.__size = size
+        Rectangle.integer_validator(self, "size", self.__size)
+
+    def area(self):
+        """ Area of square """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """ Print Square Description """
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
