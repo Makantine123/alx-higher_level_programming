e9f7532ffe8e84b5ba18a98696610c26c7b84e89
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-23 19:09:58 +0200

    Commit

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -16,4 +16,4 @@ def add_attribute(obj, att, value):
     if hasattr(obj, "__dict__"):
         setattr(obj, att, value)
     else:
-        raise TyeError("can't add new attribute")
+        raise TypeError("can't add new attribute")
