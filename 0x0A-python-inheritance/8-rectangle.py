18d1bd3ccdc8ce28659ba2b9839526d52e9e80c0
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-23 17:17:18 +0200

    Commit

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle(BaseGeometry):
     """Class Rectangle"""
     def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
         BaseGeometry.integer_validator(self, "width", self.__width)
         BaseGeometry.integer_validator(self, "height", self.__height)
+        self.__width = width
+        self.__height = height
