1fd3584d21bd88df0d87b3c0458b32d4c04c46e2
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-25 10:53:12 +0200

    Commit

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,6 +2,7 @@
 """ Returns a json representation of an object """
 import json
 
+
 def to_json_string(my_obj):
     """Function returns json representation of an object strin"""
     return (json.dumps(my_obj))
