9a1869d7740192a0dc5486fabcc2e76c0958c87d
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-26 09:35:49 +0200

    Commit

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Function that returns the dictionary description with simple data structure
+(list, dictionary, string, integer and boolean) for 
+JSON serialization of an object
+"""
+
+def class_to_json(obj):
+    """Returns the dictionary description with data structure"""
+    resdict = {}
+    objdict = obj.__dict__
+    for el in objdict:
+        if type(objdict[el]) in [list, dict, str, int, bool]:
+            resdict[el] = objdict[el]
+    return resdict
