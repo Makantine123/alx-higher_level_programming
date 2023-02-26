e3bc365fa13500c21d491b6af0312029b1f787f9
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-26 14:11:07 +0200

    Commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
-"""Makes a list from args and saves them to a file in json format"""
+"""Module 7-add_item.
+Adds all arguments to a Python list,
+and then save them to a file.
+"""
+
 import sys
 import json
-savejson = __import__("7-save_to_json_file").save_to_json_file
-loadjson = __import__("8-load_from_json_file").load_from_json_file
+import os.path
 
-try:
-    listy = loadjson("add_item.json")
-except FileNotFoundError:
-    listy = []
-for arg in sys.argv[1:]:
-    listy.append(arg)
-savejson(listy, "add_item.json")
+save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+
+my_file = 'add_item.json'
+
+my_list = []
+
+if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
+    my_list = load_from_json_file(my_file)
+
+if len(sys.argv) > 1:
+    for elem in sys.argv[1:]:
+        my_list.append(elem)
+
+save_to_json_file(my_list, my_file)
