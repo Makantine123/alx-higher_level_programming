0d0d6a59dd56ebd67d07fbbcf80fbfd720a9e96e
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-26 13:47:10 +0200

    Commit

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
-"""Module class Student"""
+"""Module 9-student.
+Creates a Student class.
+"""
 
-def __init__(self, first_name, last_name, age):
-    """Initialization of Student class"""
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
 
-def to_json(self):
+class Student:
+    """Class that defines a student.
+    Public attributes:
+        - first_name
+        - last_name
+        - age
+    Public method to_json().
     """
-    Retrieves a dictionary representation of a Student instance
-    (same as 8-class_to_json.py)
-    """
-    return self.__dict__
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes the Student instance."""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation
+        of a Student instance.
+        Returns: the dict representation of the instance.
+        """
+
+        return self.__dict__
+    
