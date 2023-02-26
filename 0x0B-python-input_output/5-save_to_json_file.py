88d84638da43e5360e2413eaf69e1955552385db
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-26 07:29:18 +0200

    Commit

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,4 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Writes object to to text file"""
-    return (json.dump(my_obj))
+    with open(filename, "w") as myfile:
+        json.dump(my_obj, myfile)
