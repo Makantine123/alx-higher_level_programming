81f035460def75dd517d3c62a5bdaedc3badbe8d
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-26 07:49:41 +0200

    Commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,7 +8,7 @@ import os
 save_to_json_file = __import__("5-save_to_json_file.py").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file.py").load_from_json_file
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     data = "add_item.json"
 
     if os.path.isfile(data):
