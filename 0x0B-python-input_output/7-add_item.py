c51d91d403e3a23159591ec0e54768e7b87b3152
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-26 09:17:42 +0200

    Commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,9 +4,9 @@ Script that loads, adds and save all arguments to a Python list
 ,then saves them to a file
 """
 import sys
+
 savefile = __import__("5-save_to_json_file.py").save_to_json_file
 loadfile = __import__("6-load_from_json_file.py").load_from_json_file
-
 
 try:
     listn = loadfile("add_item.json")
