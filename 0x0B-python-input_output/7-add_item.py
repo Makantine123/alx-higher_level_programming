ef19fa8c9bea777d83743a7b3bd3833890f663bd
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-26 14:08:45 +0200

    Commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,13 +2,15 @@
 """Makes a list from args and saves them to a file in json format"""
 import sys
 import json
-savejson = __import__("7-save_to_json_file").save_to_json_file
-loadjson = __import__("8-load_from_json_file").load_from_json_file
 
-try:
-    listy = loadjson("add_item.json")
-except FileNotFoundError:
-    listy = []
-for arg in sys.argv[1:]:
-    listy.append(arg)
-savejson(listy, "add_item.json")
+if __name__ == "__main__":
+    savejson = __import__("7-save_to_json_file").save_to_json_file
+    loadjson = __import__("8-load_from_json_file").load_from_json_file
+
+    try:
+        listy = loadjson("add_item.json")
+    except FileNotFoundError:
+        listy = []
+    for arg in sys.argv[1:]:
+        listy.append(arg)
+    savejson(listy, "add_item.json")
