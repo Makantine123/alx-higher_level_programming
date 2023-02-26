e3696059cfc5b515be6944d487942ad677db0ea8
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-26 07:55:51 +0200

    Commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,20 +2,13 @@
 """Script that loads, adds and save all arguments to a Python list
 , then saves them to a file"""
 import sys
-import os
-
-
 save_to_json_file = __import__("5-save_to_json_file.py").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file.py").load_from_json_file
 
-if __name__ == "__main__":
-    data = "add_item.json"
-
-    if os.path.isfile(data):
-        my_list = load_from_json_file(data)
-    else:
-        my_list = []
-
-    for i in range(1, len(sys.argv)):
-        my_list.append(sys.argv[i])
-    save_to_json_file(my_list, data)
+try:
+    listn = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    listn = []
+for i in sys.argv[1]:
+    listn.append(i)
+save_to_json_file(listn, "add_item.json")
