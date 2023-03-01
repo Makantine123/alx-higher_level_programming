847edadf8885eebf583f16aed810faaf1b956e3a
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-03-01 22:44:29 +0200

    Commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -105,8 +105,13 @@ class Rectangle(Base):
             x: x, 4th argument
             y: y, 5th argument
         """
-        self.id = args[0]
-        self.width = args[1]
-        self.height = args[2]
-        self.x = args[3]
-        self.y = args[4]
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
