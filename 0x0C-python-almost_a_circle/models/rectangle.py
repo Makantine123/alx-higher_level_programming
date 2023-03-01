496f0cbf299eb399624cf95822ac5cba37099617
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-03-01 21:25:20 +0200

    Commit

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Rectangle Module, inherits from Base"""
+
+
+Base = __import__("base").Base
+
+""" Base Class from base.py """
+
+class Rectangle(Base):
+    """ Rectangle class that inherits from Base """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialization of Rectangle class
+        Args:
+            width: Width of rectangle
+            height: Height of rectangle
+            x: x
+            y: y
+            id: id
+        Raises:
+            None
+        Returns:
+            None
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
