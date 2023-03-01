98e9a0e3bc2916dbe84a44a8d663e2892265345a
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-03-01 21:36:24 +0200

    Commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -23,3 +23,38 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        """The width property."""
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        self._width = value
+
+    @property
+    def height(self):
+        """The height property."""
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        self._height = value
+
+    @property
+    def x(self):
+        """The x property."""
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        self._x = value
+
+    @property
+    def y(self):
+        """The y property."""
+        return self._y
+    @y.setter
+    def y(self, value):
+        self._y = value
