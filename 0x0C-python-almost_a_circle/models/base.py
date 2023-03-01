4ed2ac75baf75a142ba327e7e1ce335ece6d3e2d
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-03-01 21:09:49 +0200

    Commit

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Module """
+
+
+class Base:
+    """ Base class, is the base of all other classess in project"""
+    
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialization of base class
+        Args:
+            id: id of instance
+        Raises:
+            None
+        Return:
+            None
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
