0140ae4e529df29b1aa7540e4fbed73720a300db
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-03-02 09:52:27 +0200

    Commit

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Module containing Square class the inherits from Rectangle"""
+from models.base import Base
+from models.base import Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherits from Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initialization of Square class
+        Args:
+            size: size
+            x: x
+            y: y
+            id: id
+        Raises:
+            None
+        Returns:
+            None
+        """
+        self.size = size
+        super().__init__(size, x=0, y=0, id)
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".\
+                format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """The size property."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
