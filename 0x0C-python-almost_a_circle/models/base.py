a27581c50db6acb2cd89bc05e5fa6a9aa4447a79
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-03-02 13:04:43 +0200

    Commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -59,8 +59,8 @@ class Base:
     def create(cls, **dictionary):
         """Returns an instance with all attributes already set"""
         if (cls.__name__ == 'Rectangle'):
-            dummy = Rectangle(1, 1)
+            dummy = cls(1, 1)
         elif (cls.__name__ == 'Square'):
-            dummy = Square(1)
+            dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
