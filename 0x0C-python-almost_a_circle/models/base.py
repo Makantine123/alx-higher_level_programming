692e4dc7cbdc9840feca6231205d226990db216e
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-03-02 15:01:46 +0200

    Commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -77,3 +77,43 @@ class Base:
                 for i in ljson:
                     mylist.append(cls.create(**i))
         return (mylist)
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Serializes list_objs in csv format, save file"""
+        if (not isinstance(list_objs, list) and list_objs is not None
+        or all(isinstance(x, cls) for x in list_objs)):
+            raise TypeError("list_objs must be a list of instances")
+
+        filename = cls.__name__ + ".csv"
+        while open(filename, "w") as myfile:
+            if list_objs is not None:
+                list_objs = [x.to_dictionary() for x in list_objs]
+                if cls.__name__ == "Rectangle":
+                    fields = ["id", "width", "height", "x", "y"]
+                elif cls.__name__ == "Square":
+                    fields = ["id", "size", "x", "y"]
+                writer = csv.DictWriter(myfile, fieldnames=fields)
+                writer.writeheader()
+                writer.writerows(list_objs)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Deserializes CSV format from file, return list of instances"""
+        filename = cls.__name__ + ".csv"
+        mylist = []
+        if os.path.exist(filename):
+            with open(filename, "r") as myfile:
+                reader = csv.reader(myfile, delimiter=", ")
+                if cls.__name__ == "Rectangle":
+                    fields = ["id", "width", "height", "x", "y"]
+                elif cls.__name__ == "Square":
+                    fields = ["id", "size", "x", "y"]
+                for x, row in enumerate(reader):
+                    if x > 0:
+                        i = cls(1, 1)
+                        for j, e in enumerate(row):
+                            if e:
+                                setattr(i, fields[j], int(e))
+                        mylist.append(i)
+        return mylist
