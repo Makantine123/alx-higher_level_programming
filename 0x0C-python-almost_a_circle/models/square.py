4f7dc08d843357d6a81074c64704c60df1697f91
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-03-02 10:29:35 +0200

    Commit

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -40,3 +40,25 @@ class Square(Rectangle):
             raise ValueError("width must be > 0")
         self.__width = value
         self.__height = value
+
+    def update(self, *args, **kwargs):
+        """
+        Assigns an argument to attributes
+        Args:
+            id: id , 1st argument
+            size: size, 2nd argument
+            x: x, 4th argument
+            y: y, 5th argument
+        """
+        if len(args) != 0:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                exec("self.{} = {}".format(key, value))
