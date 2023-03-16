6edbe1838ca6c26f59d4ac8898d73f13823b8450
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-03-16 03:56:19 +0200

    Commit

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,9 @@
--- Convert a database to utf8mb4
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Change database to utf-8
+-- Use hbtn_0c_0 database
 USE hbtn_0c_0;
-ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Change name to utf-8
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
+-- Change table to utf-8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Change database to utf-8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
