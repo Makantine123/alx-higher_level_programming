f5de4274cbcdc0ad770def84fff2096bf02cc4f5
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-03-17 01:18:48 +0200

    Commit

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,7 +4,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
 	id INT UNIQUE NOT NULL AUTO_INCREMENT,
-	state_id INT NOT,
+	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY (state_id) REFERENCES states(id)
