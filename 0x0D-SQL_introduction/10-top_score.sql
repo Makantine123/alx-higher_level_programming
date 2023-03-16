15aa58852d7e09b15778abfcadb4b960031574fb
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-03-16 03:26:56 +0200

    Commit

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- list reconds of second table
+SELECT SCORE, NAME FROM second_table ORDER SCORE DESC
---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- Updates the score of Bob to 10 in table
+UPDATE second_table SET SCORE = 10 WHERE NAME = 'Bob'
