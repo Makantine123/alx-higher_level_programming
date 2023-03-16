e1c496bf783e80cf3cf5f0086f6216c062fbf06a
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-03-17 00:49:04 +0200

    Commit

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- Creates table id_not_null
-CREATE TABLE IF NOT EXISTS id_not_null (id INT Default 1, 'name' VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
