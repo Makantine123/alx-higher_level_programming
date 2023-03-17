3492c863f708a740608a562b3d64abc9026d1aed
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-03-17 03:48:34 +0200

    Commit

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- List all cities contained in the database
 SELECT c.id, c.name, s.name
-FROM cities as c,
-INNNER JOIN states as s
+FROM cities c,
+INNNER JOIN states s
 ON c.state_id = s.id
 ORDER BY c.id ASC;
