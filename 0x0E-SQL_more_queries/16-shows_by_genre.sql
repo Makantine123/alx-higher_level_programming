be47f28b6777480e93943af187b9e17f3a4d5ad2
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-03-17 04:03:21 +0200

    Commit

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,8 @@
 --lsts all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
 -- Script
 SELECT sh.title, gr.name FROM tv_shows AS sh
-LEFT JOIN tv_show_genres AS shgr
-ON sh.id=shgr.show_id
-LEFT JOIN tv_genres AS gr
-ON shgr.genre_id=gr.id
-ORDER BY sh.title, gr.name ASC;
+	LEFT JOIN tv_show_genres AS shgr
+	ON sh.id=shgr.show_id
+	LEFT JOIN tv_genres AS gr
+	ON shgr.genre_id=gr.id
+	ORDER BY sh.title, gr.name ASC;
