eb9e33b2b82538f09a16bb2db17da06fd1b65fbc
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-12 00:56:07 +0200

    Commit

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 const sqs = parseInt(process.argv[2]);
-let shape = '#';
+let shape = 'X';
 
 if (!Number.isInteger(sqs)) {
   console.log('Missing size');
 } else {
   for (let v = 1; v < sqs; v++) {
-    shape = shape + '#';
+    shape = shape + 'X';
   }
   shape = shape.trim();
 
