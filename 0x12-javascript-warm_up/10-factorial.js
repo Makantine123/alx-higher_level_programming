4d51c29ffde2fa82d4370f416015f9e5b1dd0e1e
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-12 01:10:07 +0200

    Commit

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+function factorial (num) {
+  if (num === 1 || isNaN(num)) {
+    return (1);
+  }
+  return (factorial(num - 1) * num);
+}
+
+console.log(factorial(parseInt(process.argv[2])));
