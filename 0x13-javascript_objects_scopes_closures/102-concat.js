2826ad54c92609c9cfd34b8deec16696dbbdfa8e
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-12 16:22:01 +0200

    Commit

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
-const cmd = `cat ${process.argv[2]} ${process.argv[3]} > ${process.argv[4]}`;
+const cmd = 'cat ${process.argv[2]} ${process.argv[3]} > ${process.argv[4]}';
 require('child_process').execSync(cmd).toString('UTF-8');
