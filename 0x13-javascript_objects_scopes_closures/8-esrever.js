758d32c16ff1fde6dc410b50f2da6e8d477226f2
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-12 15:17:15 +0200

    Commit

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 
 module.exports.esrever = function (list) {
-  let nlist = [];
-  nlist = list.reverse();
+  const nlist = [];
+  let j = 0;
+
+  for (let i = list.length - 1; i >= 0; i--, j++) {
+    nlist[j] = list[i];
+  }
   return (nlist);
 };
