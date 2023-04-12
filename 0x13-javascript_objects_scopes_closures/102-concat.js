008ebc470aded8e15bec20e4b87dbe7f81ab06d2
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-12 16:17:58 +0200

    Commit

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,7 +2,7 @@
 
 let fs = require('fs');
 
-let file1 = fs.readFileSync(process.argv[2], 'utf-8');
-let file2 = fs.readFileSync(process.argv[3], 'utf-8');
+let file1 = fs.readFileSync(process.argv[2], 'utf8');
+let file2 = fs.readFileSync(process.argv[3], 'utf8');
 
 fs.write(process.argv[4], file1 + file2);
