d9dd6f9556a23ab6612daa2bcb0d8758b107695f
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-14 16:13:13 +0200

    Commit

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Script adds the State object "Louisiana" to the database """
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".
+                           format(argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = State(name="Louisiana")
+
+    session.add(state)
+    session.commit()
+
+    result = session.query(State).filter(State.name == "Louisiana").first()
+
+    if (result):
+        print("{}".format(result.id))
+    else:
+        print("Not found")
