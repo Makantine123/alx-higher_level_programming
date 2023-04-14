b4ad5ed24b41ffcb2c11f6f0c7cca61162952ede
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-14 17:33:14 +0200

    Commit

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -11,6 +11,7 @@ from sys import argv
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".
                            format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
