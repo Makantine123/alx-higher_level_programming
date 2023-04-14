50b17f8193ab698d079e8bedd65eeaf255f99517
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-14 14:50:48 +0200

    Commit

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,8 +16,8 @@ if __name__ == "__main__":
     session = Session()
 
     result = session.query(State).first()
-
-    if (result.count() == 0):
+    
+    if (result.id == ""):
         print("Nothing")
     else:
         print("{}: {}".format(result.id, result.name))
