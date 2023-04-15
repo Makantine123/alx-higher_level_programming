cf4452f73acdfc921cc08448da44c879292da654
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-15 15:45:47 +0200

    Commit

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -9,7 +9,7 @@ from relationship_state import Base, State
 from relationship_city import City
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".
                            format(argv[1],
                                   argv[2],
                                   argv[3]),
