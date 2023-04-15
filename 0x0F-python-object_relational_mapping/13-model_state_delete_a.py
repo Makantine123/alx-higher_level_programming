292bd1341be52ee021372e53782318789b1da3f2
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-16 01:20:42 +0200

    Commit

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    session = Session(engine)
+    Base.metadata.create_all(engine)
 
     state_a = session.query(State).filter(State.name.like("%a%")).all()
     for state in state_a:
