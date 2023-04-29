f947b661b0350f7518e4b656c4555a0d47eb9e09
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-29 19:41:34 +0200

    Commit

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script take in URL, sends a request to URL and displays the body of the
+response (decoded in utf-8)
+"""
+
+import urllib.error
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            data = response.read().decode('utf-8')
+            print(data)
+    except urllib.error.HTTPError as err:
+        print("Error code: {}", err.code)
