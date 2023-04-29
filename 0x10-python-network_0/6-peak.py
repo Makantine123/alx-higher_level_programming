015a29a2967eed128b3a3a61d6b9035095758195
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-29 14:50:42 +0200

    Commit

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,24 +3,6 @@
 Function finds a peak in a list of unsorted integers
 """
 
-"""
-def find_peak(list_of_integers):
-    Function finds a peak in a list
-
-    if (len(list_of_integers) == 0):
-        return None
-    elif (len(list_of_integers) == 1):
-        return list_of_integers[0]
-
-    max = list_of_integers[0]
-
-    for i in range(len(list_of_integers)):
-        if list_of_integers[i] > max:
-            max = list_of_integers[i]
-
-    return max
-"""
-
 
 def find_peak(list_of_integers):
     """Function to find a peak in a list"""
