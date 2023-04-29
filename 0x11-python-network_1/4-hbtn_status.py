c45a4e7da7a219d87873fb8ffb387cd17e40b4bb
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-29 20:35:48 +0200

    Commit

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,11 +5,9 @@ Python scriot fetches https://alx-intranet.hbtn.io/status
 
 import requests
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
-
     response = requests.get(url)
-
     print("Body response:")
     print("\t- type: ", type(response.text))
     print("\t- content: ", response.text)
