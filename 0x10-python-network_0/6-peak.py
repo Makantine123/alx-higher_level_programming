24e5bd83efbee16c24a557fd472decf8f859585b
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-29 14:20:14 +0200

    Commit

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,6 +9,8 @@ def find_peak(list_of_integers):
 
     if (len(list_of_integers) == 0):
         return None
+    elif (len(list_of_integers) == 1):
+        return list_of_integers[0]
 
     max = list_of_integers[0]
 
