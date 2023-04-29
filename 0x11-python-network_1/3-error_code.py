347523b13d5575a6edd5de74713bfbd18ea97d09
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-29 19:46:09 +0200

    Commit

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -17,4 +17,4 @@ if __name__ == "__main__":
             data = response.read().decode('utf-8')
             print(data)
     except urllib.error.HTTPError as err:
-        print("Error code: {%d}", err.code)
+        print("Error code:{}", err.code)
