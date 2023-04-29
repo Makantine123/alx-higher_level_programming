17a8d21ef54db52df814908e5c23aeb180cad0f0
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-29 14:15:26 +0200

    Commit

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,7 +7,7 @@ Function finds a peak in a list of unsorted integers
 def find_peak(list_of_integers):
     """Function finds a peak in a list"""
 
-    if (list_of_integers == []):
+    if (len(list_of_integers) == 0):
         return None
 
     max = list_of_integers[0]
