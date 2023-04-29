4f873db97c0556f8e8275ca3c4358f7d2480627a
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-29 11:15:37 +0200

    Commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL,
 # and displays the size of the body of the response
-response_size=$(curl -s -w '%(size_download)' -o /dev/null "$1")
-echo "$response_size"
+curl -sI $1 | grep "Content-Length" | awk '{print $2}'
