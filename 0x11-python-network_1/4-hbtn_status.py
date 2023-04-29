fe107854ecf07b8c43472c42fc2011f4883c3364
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-29 20:26:52 +0200

    Commit

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Python scriot fetches https://alx-intranet.hbtn.io/status
+"""
+
+import requests
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+response = requests.get(url)
+
+print("Body response:")
+print("\t- type: ", type(response.text))
+print("\t- content: ", response.text)
