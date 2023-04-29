e92ef6d1772263d66bb3995205db8c12f9c61bfe
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-29 20:48:08 +0200

    Commit

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
     response = requests.get(url)
     print("Body response:")
-    print("\t- type: ", type(response.text))
-    print("\t- content: ", response.text)
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
