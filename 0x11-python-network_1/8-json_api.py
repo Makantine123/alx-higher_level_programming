c29bd37d339ece8cebbdc7a2429ee7b25f0b4c8e
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-30 09:58:26 +0200

    Commit

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Script that takes in a letter and sends a POST request to http://0.0.0.0:5000/
+search_user with the letter as a parameter.
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+
+    if sys.argv[1]:
+        q = sys.argv[1]
+    else:
+        q = ""
+
+    data = {'q': q}
+    url = "http://0.0.0.0:500/search_user"
+
+    response = requests.post(url, data)
+
+    try:
+        json_data = response.json()
+        if len(json_data):
+            print("No result")
+        else:
+            print("[{}] {}".format(json_data["id"], json_data["name"]))
+    except ValueError:
+        print("Not a valid JSON")
