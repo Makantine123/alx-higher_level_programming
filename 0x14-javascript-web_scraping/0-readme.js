629bc1e5631ac495093b192c14ca788cc3b361e8
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-05-24 01:30:20 +0200

    Commit

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 // Script reads and prints the content of a file
 
 const fs = require('fs');
-const file = promptargv[2];
+const file = process.argv[2];
 
 fs.readFile(file, 'utf-8', (error, data)) = {
 	if (error) {
