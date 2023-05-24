85093ed1c3c19b24e878528a36bb5308ffe48947
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-05-24 02:37:08 +0200

    Commit

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
-// A script that gets the contents of a webpage and stores it in a file
 
-const fs = require('fs');
 const request = require('request');
-request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
+const fs = require('fs');
+
+request(process.argv[2], function (_err, _res, body) {
+  fs.writeFile(process.argv[3], body, 'utf8', function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
