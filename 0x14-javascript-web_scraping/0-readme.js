4189da73c0f9c399d1bf76102170dbb2420e07e6
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-05-24 02:07:46 +0200

    Commit

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', function (error, data) {
-	if (data === undefined) {
-		console.log(error);
-	} else {
-		console.log(data);
-	}
+  if (data) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
 });
