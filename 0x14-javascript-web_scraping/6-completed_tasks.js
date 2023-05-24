edd41fc41b5a56ed7028a9e23b6d2788ec1278c5
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-05-24 02:39:18 +0200

    Commit

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,17 @@
 #!/usr/bin/node
-// A script that computes the number of tasks completed by user id.
-
-const args = process.argv;
-const reqURL = args[2];
+// computes the number of tasks completed by user id.
 const request = require('request');
-request(reqURL, function (error, response, body) {
-  if (error) {
-    console.log('error:', error);
-  } else {
-    const todos = JSON.parse(body);
-    const dash = {};
-    for (let i = 0; i < todos.length; i++) {
-      const status = (todos[i].completed);
-      const key = todos[i].userId.toString();
-      if (status) {
-        if (dash[key]) {
-          dash[key]++;
-        } else {
-          dash[key] = 1;
-        }
+request(process.argv[2], (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const dict = {};
+    const arr = [];
+    JSON.parse(body).forEach((item) => {
+      if (item.completed) {
+        arr.push(item.userId);
       }
-    }
-    console.log(dash);
-  }
+      const count = arr.filter(element => element === item.userId).length;
+      dict[item.userId] = count;
+    });
+    console.log(dict);
+  } else throw error;
 });
